b581814b4e7b57c1eedcc0d76eb6a3bd1af06483
ElielRegiani <elielregiani@gmail.com>
2026-03-21 22:04:25 -0300

    first commit

---
+++ notification-api/src/main/kotlin/whatsapp_platform/notification_api/NotificationApiApplication.kt
@@ -0,0 +1,11 @@
+package whatsapp_platform.notification_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NotificationApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<NotificationApiApplication>(*args)
+}
---
+++ notification-api/src/test/kotlin/whatsapp_platform/notification_api/NotificationApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package whatsapp_platform.notification_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NotificationApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
